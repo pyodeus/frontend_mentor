b58a16c25a725f170ba39abd6d2dd85a5e9e9118
pyodeus <pyodeus@gmail.com>
2023-12-05 17:36:17 +0700

    fix button

--- 004_faq_accordion/js/main.js
+++ 004_faq_accordion/js/main.js
@@ -1,9 +1,11 @@
 const article = Array.from(document.querySelectorAll('.article'));
-const btn = Array.from(document.querySelectorAll('button'));
+const btn = Array.from(document.querySelectorAll('.article a'));
 const content = Array.from(document.querySelectorAll('.article--content'));
 
 btn.forEach((current, i) => {
-  current.addEventListener('click', () => {
+  current.addEventListener('click', (ev) => {
+    ev.preventDefault();
+
     article[i].classList.toggle('on');
     if (content[i].style.maxHeight) {
       content[i].style.maxHeight = null;
